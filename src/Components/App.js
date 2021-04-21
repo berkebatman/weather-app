defababccb93a2ff1f7458a057c58bd496c74f5d
batmanba <berke.batman@cass.city.ac.uk>
2021-04-22 00:20:13 +0100

    location rendered

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,25 @@
+/* eslint-disable react/prop-types */
 import "../Styles/App.css";
 import { React } from "react";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
 
-function App() {
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const { location } = props;
   return (
+    // eslint-disable-next-line react/jsx-no-comment-textnodes
     <div className="App">
-      <h1>Weather App</h1>
+      <LocationDetails city={location.city} country={location.country} />
     </div>
   );
-}
+};
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./Styles/index.css";
 import App from "./Components/App";
+import { location } from "./Data/forecast.json";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App location={location} />
   </React.StrictMode>,
   document.getElementById("root")
 );
